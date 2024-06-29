07db2a8f76888f339581d6fe0b1355dce3a5b27a
mirakuro <166008183+realtimesindo@users.noreply.github.com>
2024-06-29 12:46:46 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-//difftimeは秒数を指定することで、差を足し引きできる
 function GetyyyyMMddHHmmss(difftime = 0) {
     var today = new Date();
     today.setSeconds(today.getSeconds() - difftime);
@@ -17,7 +16,7 @@ function GetyyyyMMddHHmmss(difftime = 0) {
     var minute = today.getMinutes().toString();
     minute = ("0" + minute).slice(-2);
 
-    var second = today.getSeconds().toString()
+    var second = today.getSeconds().toString();
     second = ("0" + second).slice(-2);
 
     return year + month + day + hour + minute + second;
@@ -35,15 +34,6 @@ function GetyyyyMMdd(difftime = 0) {
     var day = today.getDate().toString();
     day = ("0" + day).slice(-2);
 
-    var hour = today.getHours().toString();
-    hour = ("0" + hour).slice(-2);
-
-    var minute = today.getMinutes().toString();
-    minute = ("0" + minute).slice(-2);
-
-    var second = today.getSeconds().toString()
-    second = ("0" + second).slice(-2);
-
     return year + month + day;
 }
 
@@ -59,7 +49,7 @@ L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
         attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
     }).addTo(map);
 
-//p波マーカー初期化
+// p波マーカー初期化
 var pwave = L.circle([0, 0], {
     radius: 0,
     color: 'blue',
@@ -67,7 +57,7 @@ var pwave = L.circle([0, 0], {
     fillOpacity: 0.5,
 }).addTo(map);
 
-//s波マーカー初期化
+// s波マーカー初期化
 var swave = L.circle([0, 0], {
     radius: 0,
     color: '#dc143c',
@@ -75,42 +65,34 @@ var swave = L.circle([0, 0], {
     fillOpacity: 0.1,
 }).addTo(map);
 
-
-$(function () {
+function updateMap() {
     $.when(
-        //$.getJSON(`https://weather-kyoshin.west.edge.storage-yahoo.jp/RealTimeData/${GetyyyyMMdd()}/${GetyyyyMMddHHmmss()}.json?${new Date().getTime()}`)
-        //　↑　リアルタイムで描画させる時はこのように記述するが、PC内の時間との差により取得できない場合があるため調節が必要である。
-        $.getJSON(`https://weather-kyoshin.west.edge.storage-yahoo.jp/RealTimeData/20201216/20201216140000.json`)
-        //20201216/20201216140000の部分はいずれ使えなくなるため自分で変更して確認すること。
-　　　　//https://www.data.jma.go.jp/svd/eew/data/nc/fc_hist/index.htmlここから調べられる。
+        $.getJSON(`https://weather-kyoshin.west.edge.storage-yahoo.jp/RealTimeData/${GetyyyyMMdd()}/${GetyyyyMMddHHmmss()}.json?${new Date().getTime()}`)
     ).done(function (yahoo_data) {
-        //p波とs波のデータがあるかどうか
-        if (yahoo_data.psWave === null)
-　　　　 {
-            //p波とs波がない場合は半径0mに設定して見えないようにする。
+        if (yahoo_data.psWave === null) {
             swave.setRadius(0);
             pwave.setRadius(0);
             return;
         }
-        //p波とs波のデータを取り出す。
-        let p = yahoo_data.psWave.items[0].pRadius;
-        let s = yahoo_data.psWave.items[0].sRadius;
-        //このときのp波とs波の値の単位はkmなのでmに変換させる。
-        p *= 1000;
-        s *= 1000;
+        let p = yahoo_data.psWave.items[0].pRadius * 1000;
+        let s = yahoo_data.psWave.items[0].sRadius * 1000;
 
-        //中心点(震源の緯度経度)を取り出す。
         let lat = yahoo_data.psWave.items[0].latitude.replace("N", "");
         let lng = yahoo_data.psWave.items[0].longitude.replace("E", "");
         let psCenter = new L.LatLng(lat, lng);
 
-        //s波を更新
         swave.setLatLng(psCenter);
         swave.setRadius(s);
-        //p波を更新
         pwave.setLatLng(psCenter);
         pwave.setRadius(p);
     }).fail(function () {
         console.log("error");
-    })
+    });
+}
+
+$(function () {
+    // 初回の更新
+    updateMap();
+    // 10秒ごとに更新
+    setInterval(updateMap, 10000);
 });
